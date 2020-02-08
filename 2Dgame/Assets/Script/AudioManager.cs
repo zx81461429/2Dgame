cee6ab07675324d9f8df46aaafcf1239d8e48ef1
zx81461429 <51395901+zx81461429@users.noreply.github.com>
2020-02-08 19:56:45 +0800

    1.上传开始界面
    完成了部分按钮 音效 功能

---
+++ 2Dgame/Assets/Script/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager current;
+    // Start is called before the first frame update
+    [Header("按钮音效")]
+    public AudioClip Btnenter;
+    public AudioClip Btnclick;
+
+
+
+
+    AudioSource Btnsource;
+    void Awake()
+    {
+        current = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        Btnsource = gameObject.AddComponent<AudioSource>();
+    }
+
+    public static void BtnenterSource()
+    {
+        current.Btnsource.clip = current.Btnenter;
+        current.Btnsource.Play();
+    }
+
+    public static void BtnclickSource()
+    {
+        current.Btnsource.clip = current.Btnclick;
+        current.Btnsource.Play();
+    }
+}
---
+++ 2Dgame/Assets/Script/ButtonResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ButtonResponse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MouseEnter()
+    {
+        AudioManager.BtnenterSource();
+    }
+
+}
